70bc653b78f84d95ae87ac29b5e5b4dc44e634bf
Buggu <onebuggyanothecracky@gmail.com>
2024-10-27 14:55:31 +0300

    change main

--- match_case.py
+++ match_case.py
@@ -1,4 +1,6 @@
-""" Пример работы с match case. """
+""" 
+Пример работы с match case. 
+"""
 def http_error(status):
     match status:
         case 400:
@@ -7,4 +9,5 @@ def http_error(status):
             return "Something's wrong whit the internet"
 
 if __name__ == '__main__':
-    print(http_error)
+    print(http_error(400))
+    print(http_error(418))
