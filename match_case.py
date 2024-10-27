c59494bbfe912ae4543b3e1bc8fe54473ca1d8ca
Buggu <onebuggyanothecracky@gmail.com>
2024-10-27 14:30:12 +0300

    initial

---
+++ match_case.py
@@ -0,0 +1,9 @@
+def http_error(status):
+    match status:
+        case 400:
+            return "Bad request"
+        case _:
+            return "Something's wrong whit the internet"
+
+if __name__ == '__main__':
+    print(http_error)
